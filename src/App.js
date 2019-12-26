2507e79e1bf55c1a0dcbdaaa269f991a71a2995d
geunho Kwon <geunhokwon@geunhoui-MacBook-Pro.local>
2019-12-26 18:02:22 +0900

    #done_v2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { HashRouter , Route } from "react-router-dom";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-function App(){
+function App() {
   return (
     <HashRouter>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About}/>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
-    );
+  );
 }
 
 export default App;
